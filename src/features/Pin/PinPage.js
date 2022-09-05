72fd99108d209378b1f0c0ff1aab8617af518fe0
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-09-05 20:04:22 +0700

    [FIX] Button onHandleChange

--- src/features/Pin/PinPage.js
+++ src/features/Pin/PinPage.js
@@ -4,7 +4,6 @@ import { useNavigation, useRoute } from "@react-navigation/native";
 import FormButton from "../../shared/components/FormButton";
 import { useEffect, useState } from "react";
 import { useTheme } from "../../shared/context/ThemeContext";
-import NumberView from "./components/NumberView";
 
 import ButtonNumber from "./components/ButtonNumber";
 import RowButton from "./components/RowButton";
@@ -32,7 +31,7 @@ const PinPage = () => {
 
   // handleState untuk tombol dibawah
 
-  const handleTap = (type, value) => {
+  const onHandleChange = (type, value) => {
     if (type === "number") {
       setCurrVal(`${currVal}${value}`);
     }
@@ -47,43 +46,6 @@ const PinPage = () => {
       setCurrVal("0");
       setOperator(null);
       setPrevVal(null);
-    }
-
-    if (type === "posneg") {
-      setCurrVal(`${parseFloat(currVal) * -1}`);
-    }
-
-    if (type === "percentage") {
-      setCurrVal(`${parseFloat(currVal) * 0.01}`);
-    }
-
-    if (type === "equal") {
-      const current = parseFloat(currVal);
-      const previous = parseFloat(prevVal);
-
-      if (operator === "+") {
-        setCurrVal(previous + current);
-        setOperator(null);
-        setPrevVal(null);
-      }
-
-      if (operator === "/") {
-        setCurrVal(previous / current);
-        setOperator(null);
-        setPrevVal(null);
-      }
-
-      if (operator === "-") {
-        setCurrVal(previous - current);
-        setOperator(null);
-        setPrevVal(null);
-      }
-
-      if (operator === "*") {
-        setCurrVal(previous * current);
-        setOperator(null);
-        setPrevVal(null);
-      }
     }
   };
 
@@ -102,7 +64,7 @@ const PinPage = () => {
             Please input PIN {"\n"} (User id : {pinParam.userId})
           </Text>
 
-          {/* <TextInput
+          <TextInput
             secureTextEntry
             style={{
               borderBottomColor: theme.colors.foreground,
@@ -111,26 +73,11 @@ const PinPage = () => {
               fontSize: 32,
               textAlign: "center",
             }}
-            value={pin}
-            onChangeText={setPin}
-          ></TextInput> */}
-
-          {/* <TextInput
-            secureTextEntry
-            style={{
-              borderBottomColor: theme.colors.foreground,
-              borderBottomWidth: 1,
-              marginVertical: theme.spacing.l,
-              fontSize: 32,
-              textAlign: "center",
-            }}
-            value= {currVal}
-            onChangeText={setPin}
+            value={currVal}
+            onChangeText={setCurrVal}
           >
-            {currVal}
-          </TextInput> */}
-
-          <Text style={styles.value}>{currVal}</Text>
+            {/* {currVal} */}
+          </TextInput>
         </View>
       </View>
 
@@ -143,29 +90,29 @@ const PinPage = () => {
       {/* tombol yang baru dibawah */}
 
       <RowButton>
-        <ButtonNumber text="7" onPress={() => handleTap("number", 7)} />
-        <ButtonNumber text="8" onPress={() => handleTap("number", 8)} />
-        <ButtonNumber text="9" onPress={() => handleTap("number", 9)} />
+        <ButtonNumber text="7" onPress={() => onHandleChange("number", 7)} />
+        <ButtonNumber text="8" onPress={() => onHandleChange("number", 8)} />
+        <ButtonNumber text="9" onPress={() => onHandleChange("number", 9)} />
       </RowButton>
 
       <RowButton>
-        <ButtonNumber text="4" onPress={() => handleTap("number", 4)} />
-        <ButtonNumber text="5" onPress={() => handleTap("number", 5)} />
-        <ButtonNumber text="6" onPress={() => handleTap("number", 6)} />
+        <ButtonNumber text="4" onPress={() => onHandleChange("number", 4)} />
+        <ButtonNumber text="5" onPress={() => onHandleChange("number", 5)} />
+        <ButtonNumber text="6" onPress={() => onHandleChange("number", 6)} />
       </RowButton>
 
       <RowButton>
-        <ButtonNumber text="1" onPress={() => handleTap("number", 1)} />
-        <ButtonNumber text="2" onPress={() => handleTap("number", 2)} />
-        <ButtonNumber text="3" onPress={() => handleTap("number", 3)} />
+        <ButtonNumber text="1" onPress={() => onHandleChange("number", 1)} />
+        <ButtonNumber text="2" onPress={() => onHandleChange("number", 2)} />
+        <ButtonNumber text="3" onPress={() => onHandleChange("number", 3)} />
       </RowButton>
 
       <RowButton>
-        <ButtonNumber text="0" onPress={() => handleTap("number", 0)} />
+        <ButtonNumber text="0" onPress={() => onHandleChange("number", 0)} />
         <ButtonNumber
           text="C"
           theme="secondary"
-          onPress={() => handleTap("clear")}
+          onPress={() => onHandleChange("clear")}
         />
       </RowButton>
 
@@ -173,7 +120,7 @@ const PinPage = () => {
 
       <FormButton
         onClick={() => {
-          console.log(pin);
+          console.log(currVal);
           navigation.navigate(pinParam.prevPage, {
             message: "ok",
           });
