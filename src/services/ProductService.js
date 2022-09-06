0fac1b955ba1c8b61e15dbdf822ffa925ac170b7
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-09-06 13:43:17 +0700

    [ADD] Product Axios Client and Service

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,25 +1,24 @@
-import {sleep} from "../shared/sleep";
-import {DATA} from "../data/products";
+import { sleep } from "../shared/sleep";
+import { DATA } from "../data/products";
 
-export const productService = () => {
-    const getAllProduct = async (page) => {
-        const pagePerRow = 20;
-        const startIndex = (page - 1) * pagePerRow;
-        const endIndex = page * pagePerRow;
-        try {
-            return await sleep((resolve, reject) => {
-                const response = DATA.slice(startIndex, endIndex)
-                if (response.length === 0) {
-                    reject('no more data')
-                } else {
-                    resolve(response);
-                }
+import { SERVICE } from "../shared/constants";
 
-            }, 1000);
-        } catch (e) {
-            throw e;
-        }
+export const productService = ({ doGet }) => {
+  const getAllProduct = async (page) => {
+    try {
+      const response = await doGet({
+        url: SERVICE.PRODUCT,
+        params: { pageNo: page, itemPerPage: 20 },
+      });
+      if (response.products.length === 0) {
+        throw new Error("No Data");
+      } else {
+        return response.products;
+      }
+    } catch (e) {
+      throw e;
     }
+  };
 
-    return {getAllProduct}
-}
+  return { getAllProduct };
+};
--- src/shared/AxiosClient.js
+++ src/shared/AxiosClient.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import Constants from "expo-constants";
+import { authHeaderInterceptor } from "./interceptor/AuthHeaderInterceptor";
 
 export const clientInstance = axios.create({
   baseURL: Constants.manifest.extra.baseURL,
 });
+
+clientInstance.interceptors.request.use(authHeaderInterceptor);
